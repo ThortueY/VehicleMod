ae1055fd334d7d73ff0b75c21aecefb285fa48bb
Louis PAQUEREAU <louis.paquereau@etudiant.univ-lr.fr>
2026-02-22 21:49:25 -0500

    Edited item render on inventory

--- src/main/java/fr/frankulinn/vehiclemod/client/renderer/SeatItemRenderer.java
+++ src/main/java/fr/frankulinn/vehiclemod/client/renderer/SeatItemRenderer.java
@@ -1,11 +1,32 @@
 package fr.frankulinn.vehiclemod.client.renderer;
 
+import com.mojang.blaze3d.vertex.PoseStack;
+import com.mojang.math.Axis;
 import fr.frankulinn.vehiclemod.client.model.SeatItemModel;
 import fr.frankulinn.vehiclemod.item.SeatItem;
+import net.minecraft.client.renderer.MultiBufferSource;
+import net.minecraft.world.item.ItemDisplayContext;
+import net.minecraft.world.item.ItemStack;
 import software.bernie.geckolib.renderer.GeoItemRenderer;
 
 public class SeatItemRenderer extends GeoItemRenderer<SeatItem> {
     public SeatItemRenderer() {
         super(new SeatItemModel());
+    }
+
+    @Override
+    public void renderByItem(ItemStack stack, ItemDisplayContext transformType, PoseStack poseStack,
+            MultiBufferSource bufferSource, int packedLight, int packedOverlay) {
+        if (transformType == ItemDisplayContext.GUI || transformType == ItemDisplayContext.FIXED) {
+            poseStack.pushPose();
+            poseStack.translate(0.5, 0.25, 0.5);
+            poseStack.mulPose(Axis.XP.rotationDegrees(30));
+            poseStack.mulPose(Axis.YP.rotationDegrees(225));
+            poseStack.translate(-0.5, -0.5, -0.5);
+            super.renderByItem(stack, transformType, poseStack, bufferSource, packedLight, packedOverlay);
+            poseStack.popPose();
+        } else {
+            super.renderByItem(stack, transformType, poseStack, bufferSource, packedLight, packedOverlay);
+        }
     }
 }
--- src/main/java/fr/frankulinn/vehiclemod/client/renderer/WheelItemRenderer.java
+++ src/main/java/fr/frankulinn/vehiclemod/client/renderer/WheelItemRenderer.java
@@ -1,11 +1,32 @@
 package fr.frankulinn.vehiclemod.client.renderer;
 
+import com.mojang.blaze3d.vertex.PoseStack;
+import com.mojang.math.Axis;
 import fr.frankulinn.vehiclemod.client.model.WheelItemModel;
 import fr.frankulinn.vehiclemod.item.WheelItem;
+import net.minecraft.client.renderer.MultiBufferSource;
+import net.minecraft.world.item.ItemDisplayContext;
+import net.minecraft.world.item.ItemStack;
 import software.bernie.geckolib.renderer.GeoItemRenderer;
 
 public class WheelItemRenderer extends GeoItemRenderer<WheelItem> {
     public WheelItemRenderer() {
         super(new WheelItemModel());
+    }
+
+    @Override
+    public void renderByItem(ItemStack stack, ItemDisplayContext transformType, PoseStack poseStack,
+            MultiBufferSource bufferSource, int packedLight, int packedOverlay) {
+        if (transformType == ItemDisplayContext.GUI || transformType == ItemDisplayContext.FIXED) {
+            poseStack.pushPose();
+            poseStack.translate(0.5, 0.5, 0.5);
+            poseStack.mulPose(Axis.XP.rotationDegrees(30));
+            poseStack.mulPose(Axis.YP.rotationDegrees(225));
+            poseStack.translate(-0.5, -0.5, -0.5);
+            super.renderByItem(stack, transformType, poseStack, bufferSource, packedLight, packedOverlay);
+            poseStack.popPose();
+        } else {
+            super.renderByItem(stack, transformType, poseStack, bufferSource, packedLight, packedOverlay);
+        }
     }
 }
